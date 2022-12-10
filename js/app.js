d49b7170401d4d837c54fed3d50a5f6647a68d8f
Alex Silva <alexsilvaddt17@gmail.com>
2022-12-10 19:57:33 -0300

    Agregué un mensaje de error en caso de que no se rellenen los campos que solicita la página

--- js/app.js
+++ js/app.js
@@ -9,5 +9,40 @@ window.addEventListener("load", () => {
 function buscarClima(evt) {
     evt.preventDefault();
 
-    console.log("Buscando el clima");
+    // Validar 
+    const ciudad = document.querySelector("#ciudad").value;
+    const pais = document.querySelector("#pais").value;
+
+    if (ciudad === "" || pais === "") {
+        // Datos invalidos
+        mostrarError("Por favor, ingresa el nombre de la ciudad y selecciona el país para conocer el clima");
+
+        return;
+    }
+    // Consultar la API
+}
+
+function mostrarError(mensaje) {
+    const alerta = document.querySelector(".bg-red-100");
+
+    if (!alerta) {
+        // Mensaje de error
+        const alerta = document.createElement("div");
+
+        alerta.classList.add("bg-red-100", "border-red-400", "text-red-700", "px-4", "py-3", "rounded", "max-w-md", "mx-auto", "mt-6", "text-center");
+
+        alerta.innerHTML = `
+        <strong class="font-bold">Error!</strong>
+        <span class="block">${mensaje}</span>
+    `;
+
+        container.appendChild(alerta);
+
+        // Eliminar el mensaje de error después de cinco segundos con setTimeout
+        setTimeout(() => {
+            alerta.remove();
+        }, 5000);
+    }
+
+
 }
